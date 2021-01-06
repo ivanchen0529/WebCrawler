289c3005580dd54944201e19c44c3c052d873c21
ivanchen0529 <57256864+ivanchen0529@users.noreply.github.com>
2021-01-06 21:53:55 +0800

    second

--- first.py
+++ first.py
@@ -2,5 +2,19 @@ import requests
 
 r = requests.get('http://www.baidu.com')
 r.encoding = r.apparent_encoding
-print(r.status_code,r.text)
-print(r.headers)
+# print(r.status_code,r.text)
+# print(r.headers)
+
+# 爬取网页的通用代码框架
+def getHTMLText(url):
+    try:
+        r = requests.get(url, timeout=30)
+        r.raise_for_status()    # 如果状态不是200，引发HTTPError异常
+        r.encoding = r.apparent_encoding
+        return r.text
+    except:
+        return "产生异常"
+
+if __name__=="__main__":
+    url = "www.baidu.com"
+    print(getHTMLText(url))
